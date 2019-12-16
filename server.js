696fd40dcdb962dddc8470c019c3e62bf7e5dcf2
Tahir <tahir.ali@vroozi.com>
2019-12-16 16:39:07 +0500

    https

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = 443;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
